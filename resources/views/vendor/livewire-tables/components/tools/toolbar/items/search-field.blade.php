5f1ddb11c4cf3a23312c5e57749f4315446bb552
banban <kjy0939@gmail.com>
2025-10-25 23:38:31 +0900

    검색창 반응형 너비 개선
    - 768px 미만: w-full로 전체 너비 사용
    - 768px 이상 (md): max-w-xs (20rem)
    - 1024px 이상 (lg): max-w-sm (24rem)
    - 1280px 이상 (xl): max-w-md (28rem)
    - 모바일에서 검색창이 전체 너비를 활용하여 사용성 향상

--- resources/views/vendor/livewire-tables/components/tools/toolbar/items/search-field.blade.php
+++ resources/views/vendor/livewire-tables/components/tools/toolbar/items/search-field.blade.php
@@ -1,6 +1,6 @@
 @aware(['isTailwind', 'isBootstrap'])
 
-<div class="relative w-full max-w-xs sm:max-w-sm md:max-w-md">
+<div class="relative w-full md:max-w-xs lg:max-w-sm xl:max-w-md">
     @if($this->hasSearchIcon)
         <x-livewire-tables::tools.toolbar.items.search.icon :searchIcon="$this->getSearchIcon" :searchIconClasses="$this->getSearchIconClasses" :searchIconOtherAttributes="$this->getSearchIconOtherAttributes"  />
     @endif
